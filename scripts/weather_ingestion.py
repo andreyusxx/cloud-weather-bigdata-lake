d59be150aff23a41ee2e23b350fd340c1d50e46e
Шарагін Андрій <andrijsaragin@gmail.com>
2026-02-23 14:26:22 +0200

    feat: integrate boto3 and connect script to MinIO storage

--- scripts/weather_ingestion.py
+++ scripts/weather_ingestion.py
@@ -1,7 +1,11 @@
+
 import requests
 import json
 import os
+from datetime import datetime
 from dotenv import load_dotenv
+import boto3
+from botocore.client import Config
 
 load_dotenv()
 
@@ -9,19 +13,26 @@ API_KEY = os.getenv("OPENWEATHER_API_KEY")
 CITY = "Kyiv"
 URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
+
+s3 = boto3.resource('s3',
+                    endpoint_url='http://localhost:9000',
+                    aws_access_key_id='admin',
+                    aws_secret_access_key='password',
+                    config=Config(signature_version='s3v4'),
+                    region_name='us-east-1')
+
 def fetch_weather():
-    if not API_KEY:
-        print("Помилка: Ключ не знайдено! Перевір файл .env")
-        return
-    
+
     response = requests.get(URL)
-    if response.status_code == 200:
-        data = response.json()
-        print(f"Погода у місті {CITY}:")
-        print(f"Температура: {data['main']['temp']}°C")
-        print(f"Вологість: {data['main']['humidity']}%")
-    else:
-        print(f"Помилка: {response.status_code}")
+    response.raise_for_status() 
+    data = response.json()
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"weather_{timestamp}.json"
+
+    bucket_name = 'weather-data'
+    s3.Object(bucket_name, f"raw/{filename}").put(Body=json.dumps(data, indent=4))
+    
 
 if __name__ == "__main__":
     fetch_weather()
