68ba5e01c00538e85844b9171284b2a2aa828b58
Шарагін Андрій <andrijsaragin@gmail.com>
2026-02-25 11:57:42 +0200

    feat: expand data collection to multiple cities

--- scripts/weather_ingestion.py
+++ scripts/weather_ingestion.py
@@ -10,8 +10,8 @@ from botocore.client import Config
 load_dotenv()
 
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
-CITY = "Kyiv"
-URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+CITIES = ["Kyiv", "Lviv", "Odesa", "Kharkiv", "Dnipro"]
+
 
 
 s3 = boto3.resource('s3',
@@ -22,17 +22,21 @@ s3 = boto3.resource('s3',
                     region_name='us-east-1')
 
 def fetch_weather():
+    for city in CITIES:
+        URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        try:
+            response = requests.get(URL)
+            response.raise_for_status() 
+            data = response.json()
 
-    response = requests.get(URL)
-    response.raise_for_status() 
-    data = response.json()
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"weather_{city}_{timestamp}.json"
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = f"weather_{timestamp}.json"
-
-    bucket_name = 'weather-data'
-    s3.Object(bucket_name, f"raw/{filename}").put(Body=json.dumps(data, indent=4))
-    
+            bucket_name = 'weather-data'
+            s3.Object(bucket_name, f"raw/{filename}").put(Body=json.dumps(data, indent=4))
+            print(f"✅ Дані для {city} збережено!")
+        except Exception as e:
+            print(f"❌ Помилка для {city}: {e}")
 
 if __name__ == "__main__":
     fetch_weather()
